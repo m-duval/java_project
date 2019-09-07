3eb599649a9faff963610ff426ff5f70e5c6ed1c
MICHAEL DUVAL <duv74@hotmail.com>
2019-09-07 15:49:27 -0400

    final update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,32 +6,17 @@ function buildTable(data){
     tbody.html("");
     
     data.forEach((dataRow) => {
-        
         let row = tbody.append("tr");
         
         Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
-
+            
             cell.text(val);
 
         });
-        
+
     })
-}
-
-function handleClick(){
-    let date = d3.select("#datetime").property("value");
-
-    let filterData = tableData;
-
-    if(date) {
-        filterData = filterData.filter((row) => row.datetime === date);
-    }
     
-    buildTable(filterData);
-
 }
-
-d3.selectAll("#filter-btn").on("click", handleClick);
 
 buildTable(tableData);

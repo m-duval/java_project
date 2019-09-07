63f8da2977d000b30f9ae035e2b9857b56d2fb1d
MICHAEL DUVAL <duv74@hotmail.com>
2019-09-07 15:18:42 -0400

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,20 @@
-// from data.js
+let tbody = d3.select("tbody");
+
 var tableData = data;
 
-// YOUR CODE HERE!
+function buildTable(data){
+    tbody.html("");
+    
+    data.forEach((dataRow) => {
+        
+        let row = tbody.append("tr");
+        
+        Object.values(dataRow).forEach((val) => {
+            
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    })
+}
+
+buildTable(tableData);
